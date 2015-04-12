df04bc917bf03b745c50de472eb91759984b6b9f
kinescript <yuma.ohfusa@gmail.com>
2015-04-13 03:17:19 +0900

    モーダル追加, mobile対応

--- js/script.js
+++ js/script.js
@@ -4,10 +4,14 @@ var DKTDesk = function() {
 	self.deskWidth;
 	self.deskHeight;
 	self.materials;
+	self.objectClickbehavior;
 
 
 	function init() {
 		self.paper = Raphael("content", $("#content").width(), $("#content").height());
+		self.objectClickbehavior = function(url) {
+			location.href = url;
+		};
 	}
 
 
@@ -22,12 +26,15 @@ var DKTDesk = function() {
 	self.draw = function() {
 		if(!self.deskWidth || !self.deskHeight || !self.materials) return;
 
-		var ww = $("#content").width();
-		var wh = $("#content").height();
+		var ww = window.innerWidth;
+		var wh = window.innerHeight;
 		var ratio = Math.max(ww/self.deskWidth, wh/self.deskHeight);
 
 		self.paper.clear();
-		self.paper.setSize(ww, wh);
+		self.paper.setSize(self.deskWidth*ratio, self.deskHeight*ratio);
+
+		$("#wrapper").width(self.deskWidth*ratio);
+		$("#wrapper").height(self.deskHeight*ratio);
 
 		for(var i = 0; i < self.materials.length; i ++) {
 			var material = self.materials[i];
@@ -43,7 +50,8 @@ var DKTDesk = function() {
 			var obj = self.paper.path(pathString).attr({
 				fill: "red",
 				'fill-opacity': 0,
-				cursor: 'pointer'
+				cursor: 'pointer',
+				//stroke: "none"
 			});
 
 			obj.node.id = material.url; 
@@ -53,7 +61,8 @@ var DKTDesk = function() {
 			}).mouseout(function(e) {
 				// console.log("out")
 			}).mouseup(function(e) {
-				location.href = e.toElement.id;
+				var url = e.toElement.id;
+				self.objectClickbehavior(url);
 			});
 		}
 	}
@@ -63,12 +72,102 @@ var DKTDesk = function() {
 };
 
 
+var DKTModal = function() {
+	var self = this;
+	var modalContainer = $("#modal");
+	var modalWindow = $("#window");
+	var modalLoading = $("#modalLoading");
+	var modalBG = $("#modalBg");
+	var modalClose = $("#modalClose");
+	var deskScrollTop = 0;
+	var deskScrollLeft = 0;
+	var isModalVisible = false;
+
+
+	function init() {
+		modalContainer.hide();
+		modalBG.on("click", self.closeModal);
+		modalClose.on("click", self.closeModal);
+	}
+
+
+	function attachArticle(html) {
+		if(!isModalVisible) return;
+
+		modalWindow.find("#windowInner").html($("<div>").append($.parseHTML(html)).find("#article_container"));
+		modalLoading.animate({opacity:0}, 500, "easeOutExpo", function(){
+			modalWindow.show();
+		});
+	}
+
+
+	self.openModal = function(url) {
+		isModalVisible = true;
+		modalContainer.show();
+		modalWindow.hide();
+		modalLoading.stop(true).css("opacity", 1).show();
+		modalBG.css("opacity", 0);
+		modalBG.animate({opacity:0.8}, 300, "easeOutExpo");
+
+		deskScrollTop = $("body").scrollTop();
+		deskScrollLeft = $("body").scrollLeft();
+		$("#wrapper").css({
+			top: -deskScrollTop,
+			left: -deskScrollLeft
+		});
+		$("html,body").css({
+			overflow:"hidden",
+			width: "100%",
+			height: "100%"
+		});
+
+		$.ajax({
+			type: 'GET',
+			url: url,
+			dataType: 'html',
+			success: function(html){
+				attachArticle(html);
+			}, 
+			error: function() {
+				self.closeModal();
+			}
+		});
+	}
+
+
+	self.closeModal = function() {
+		isModalVisible = false;
+		modalWindow.hide();
+		modalLoading.hide();
+		modalBG.stop(true).animate({opacity:0}, 300, "linear", function() {
+			modalContainer.hide();
+			$("html,body").css({
+				overflow:"visible",
+				width: "auto",
+				height: "auto"
+			});
+			$("body").scrollTop(deskScrollTop).scrollLeft(deskScrollLeft);
+			$("#wrapper").css({
+				top: "auto",
+				left: "auto"
+			});
+		});
+	}
+
+
+	init();
+}
+
+
 $(function() {
 	var desk = new DKTDesk();
+	var modal = new DKTModal();
 	var handler = new DKTHandler();
 
+	desk.objectClickbehavior = modal.openModal;
+
 	/*--- DKTリスナ設定 ---*/
-	handler.listen(desk.update, 1000);
+	handler.listen(desk.update, 10000);
 
 	/*--- リサイズハンドラバインド ---*/
 	$(window).on("resize", desk.draw);
